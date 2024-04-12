95823ad0a48658fdaa025d130be97828176d659e
Sebastiano Francia <SEBASTIANO.FRANCIA@local.ittcesena.it>
2024-04-12 10:40:18 +0200

    update pj

---
+++ Solitaire-Treandy-Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Solitaire_Treandy_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- Solitaire-Trendy-WPF/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ Solitaire-Trendy-WPF/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace Solitaire_Trendy_WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Solitaire-Trendy-WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Solitaire-Trendy-WPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
